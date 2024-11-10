37c247bd5198c86e85d112c8da81350dac89f1f9
tadeastehan <tadeastehan@gmail.com>
2024-11-10 21:45:35 +0100

    Pairing between slave and hub. Slave can only except cal. data from hub.

--- components/espnow_send_custom_data/espnow_send_custom_data.cpp
+++ components/espnow_send_custom_data/espnow_send_custom_data.cpp
@@ -1,7 +1,13 @@
 #include "espnow_send_custom_data.h"
 
 std::vector<unsigned char> format_espnow_data(const char* device_id, const char* sensor_id, float sensor_value) {
-    char buffer[200];
+    char buffer[241];
     snprintf(buffer, sizeof(buffer), "%s;%s;%.0f", device_id, sensor_id, sensor_value);
+    return std::vector<unsigned char>(buffer, buffer + strlen(buffer));
+}
+
+std::vector<unsigned char> format_espnow_data_new(const char* sensor_id, float sensor_value) {
+    char buffer[241];
+    snprintf(buffer, sizeof(buffer), "%s;%.0f;", sensor_id, sensor_value);
     return std::vector<unsigned char>(buffer, buffer + strlen(buffer));
 }
--- components/espnow_send_custom_data/espnow_send_custom_data.h
+++ components/espnow_send_custom_data/espnow_send_custom_data.h
@@ -6,5 +6,7 @@
 #include <cstring>
 
 std::vector<unsigned char> format_espnow_data(const char* device_id, const char* sensor_id, float sensor_value);
+std::vector<unsigned char> format_espnow_data_new(const char* sensor_id, float sensor_value);
+
 
 #endif // ESPNOW_SEND_CUSTOM_DATA_H
