856b06b17dc79d18f95c5c2216379356a6025d02
tadeastehan <tadeastehan@gmail.com>
2024-11-12 16:38:27 +0100

    Functionality to add pinged devices only when pinged devices is in pairing mode. (pairing button pressed for more than 3 seconds.

--- components/espnow_send_custom_data/espnow_send_custom_data.cpp
+++ components/espnow_send_custom_data/espnow_send_custom_data.cpp
@@ -2,7 +2,7 @@
 
 std::vector<unsigned char> format_espnow_data(const char* device_id, const char* sensor_id, float sensor_value) {
     char buffer[241];
-    snprintf(buffer, sizeof(buffer), "%s;%s;%.0f", device_id, sensor_id, sensor_value);
+    snprintf(buffer, sizeof(buffer), "%s;%s;%.0f;", device_id, sensor_id, sensor_value);
     return std::vector<unsigned char>(buffer, buffer + strlen(buffer));
 }
 
