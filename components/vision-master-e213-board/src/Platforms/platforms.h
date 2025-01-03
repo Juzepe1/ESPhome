af822c1dc6b310b6073593bef9009b8cf2d826d9
tadeastehan <tadeastehan@gmail.com>
2025-01-03 22:52:25 +0100

    Added a funcional transformed arduino library to only work with Heltec Vision Master E213. TO DO: Implement it with esphome.

---
+++ components/vision-master-e213-board/src/Platforms/platforms.h
@@ -0,0 +1,13 @@
+// All platforms, as one header
+
+#ifndef __PLATFORMS_H__
+#define __PLATFORMS_H__
+
+#include <Arduino.h>
+
+// Supported
+#include "Platforms/VisionMasterE213/VisionMasterE213.h"
+
+#include "Platforms/ESP32/ESP32.h"
+
+#endif
