4156a84fac2877625770129b9b2c81206268b7f6
tadeastehan <tadeastehan@gmail.com>
2025-01-03 22:52:25 +0100

    Added a funcional transformed arduino library to only work with Heltec Vision Master E213. TO DO: Implement it with esphome.

---
+++ components/vision-master-e213-board/src/vision-master-e213-board.h
@@ -0,0 +1,14 @@
+#ifndef __VISION_MASTER_E213_BOARD_H__
+#define __VISION_MASTER_E213_BOARD_H__
+
+#include <Arduino.h>
+#include "GFX_Root/GFX.h"
+
+#include "Platforms/platforms.h"
+
+#include "Displays/class-aliases.h" // Rename displays, for backwards compatibility
+
+// All-in-one "Wireless Paper" boards
+#include "Displays/LCMEN2R13EFC1/LCMEN2R13EFC1.h"
+
+#endif
